80a1302348fa55215195f8f2f39ef74552016419
Michael Herman <hermanmu@gmail.com>
2019-11-25 10:44:58 -0700

    bump dependencies, lint, update readme

--- README.md
+++ README.md
@@ -10,4 +10,8 @@ Spin up the containers:
 $ docker-compose up -d --build
 ```
 
-Open your browser to http://localhost:5004
+Open your browser to http://localhost:5004 to view the app or to http://localhost:9181 to view the RQ dashboard.
+
+### Want to learn how to build this?
+
+Check out the [post](https://testdriven.io/asynchronous-tasks-with-flask-and-redis-queue).
--- manage.py
+++ manage.py
@@ -3,6 +3,8 @@
 
 import unittest
 
+import redis
+from rq import Connection, Worker
 from flask.cli import FlaskGroup
 
 from project.server import create_app
@@ -20,6 +22,15 @@ def test():
     if result.wasSuccessful():
         return 0
     return 1
+
+
+@cli.command("run_worker")
+def run_worker():
+    redis_url = app.config["REDIS_URL"]
+    redis_connection = redis.from_url(redis_url)
+    with Connection(redis_connection):
+        worker = Worker(app.config["QUEUES"])
+        worker.work()
 
 
 if __name__ == "__main__":
