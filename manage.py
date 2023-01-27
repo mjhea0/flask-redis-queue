3edaffaa47cbbace0e9864ea8accfadb97198b4b
Marc <Marc@localhost>
2023-01-28 00:13:18 +0100

    aaa

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 # manage.py
-
+import redis
+from rq import Connection, Worker
 
 import unittest
 
@@ -20,6 +21,14 @@ def test():
     if result.wasSuccessful():
         return 0
     return 1
+
+@cli.command("run_worker")
+def run_worker():
+    redis_url = app.config["REDIS_URL"]
+    redis_connection = redis.from_url(redis_url)
+    with Connection(redis_connection):
+        worker = Worker(app.config["QUEUES"])
+        worker.work()
 
 
 if __name__ == "__main__":
